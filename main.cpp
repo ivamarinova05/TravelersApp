a27d2c71097a8772567d1530efe94d28a661f03d
unknown <iva_m_marinova@abv.bg>
2021-10-14 22:17:22 +0300

    main added

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include "Trip.h"
+#include "Person.h"
+#include "Session.h"
+#include "CLR.h"
+#include "Validation.h"
+
+#include <iostream>
+int main()
+{
+    CLR clr;
+    clr.read();
+    
+    //user.printPerson();
+
+
+    return 0;
+}
